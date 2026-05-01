f82a348c21a5ab6ab83a238cc610cfeaad6b3566
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2026-05-02 01:41:04 +0300

    add weather entity

---
+++ lib/weather/domain/entities/weather_entity.dart
@@ -0,0 +1,11 @@
+class WeatherEntity {
+  final String cityName;
+  final String weatherCondition;
+  final double tempC;
+
+  WeatherEntity({
+    required this.cityName,
+    required this.weatherCondition,
+    required this.tempC,
+  });
+}
